fc4111661ce9acfd3cfb8107a8798f67d0650c5a
Scott Hardin <shardin2@ncsu.edu>
2023-09-20 15:52:58 -0400

    Add routes links

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -34,4 +34,10 @@ class ArticlesController < ApplicationController
   def edit
     @article = Article.find(params[:id])
   end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
 end
