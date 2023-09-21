14aad7a2052ad0a3699b8e6ef5986a1a502a07fe
Scott Hardin <shardin2@ncsu.edu>
2023-09-21 13:04:47 -0400

    Add route for signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
+
   has_many :articles
 
   validates :username, presence: true,
@@ -12,4 +14,5 @@ class User < ApplicationRecord
             length: {maximum: 105},
             format: { with: VALID_EMAIL_REGEX }
 
+  has_secure_password
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root 'articles#index'
   get 'about', to: 'pages#about'
   resources :articles
+  get 'signup', to: 'users#new'
+  post 'users', to: 'users#create'
 end
