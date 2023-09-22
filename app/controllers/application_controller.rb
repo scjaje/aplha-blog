5bd4d4392b3f4a79c5c8f74a5d9d0cd1de6477c3
Scott Hardin <shardin2@ncsu.edu>
2023-09-21 22:52:02 -0400

    Add controller methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+
+  # makes it available to views, too
+  helper_method :current_user, :logged_in?
+  def current_user
+    # returns @current_user if the User.find has been validated.
+    # If not, it will query the database.
+    # This stops unnecessary querying.
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    # !! turns current user into a bool
+    !!current_user
+  end
+
 end
