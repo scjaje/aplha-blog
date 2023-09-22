e2564d9859e31ac1423b6d72bae6cb25bccd5b7a
Scott Hardin <shardin2@ncsu.edu>
2023-09-22 00:16:38 -0400

    Add admin access to views and controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,7 +42,7 @@ class UsersController < ApplicationController
   def destroy
     @user.destroy
     # important or will cause issues with session id
-    session[:user_id] = nil
+    session[:user_id] = nil if @user == current_user
     flash[:notice] = "Account and all associated articles successfully deleted"
     redirect_to articles_path
   end
@@ -57,7 +57,7 @@ class UsersController < ApplicationController
   end
 
   def require_same_user
-    if current_user != @user
+    if current_user != @user && !current_user.admin?
       flash[:alert] = "You can only edit or delete your own profile."
       redirect_to @user
     end
