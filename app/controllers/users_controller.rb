cb068803bf743b9727ea1ab593a0f7ed827eab92
Scott Hardin <shardin2@ncsu.edu>
2023-09-21 22:36:10 -0400

    Add Authentication helper methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-
+  before_action :set_user, only: [:show, :edit, :update]
   def new
     @user = User.new
   end
@@ -7,6 +7,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      # signs a user in once they sign up
+      session[:user_id] = @user.id
       flash[:notice] = "Welcome #{@user.username}! You have successfully signed up. "
       redirect_to articles_path
     else
@@ -15,11 +17,10 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
+
     if @user.update(user_params)
       flash[:notice] = "Your account information was successfully update."
       redirect_to @user
@@ -29,17 +30,19 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
     @articles = @user.articles.paginate(:page => params[:page], :per_page => 3)
   end
 
   def index
     @users = User.paginate(:page => params[:page], :per_page => 3)
-
   end
 
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,17 @@ module ApplicationHelper
     gravatar_url = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
     image_tag(gravatar_url, alt: user.username)
   end
+
+  def current_user
+    # returns @current_user if the User.find has been validated.
+    # If not, it will query the database.
+    # This stops unnecessary querying.
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    # !! turns current user into a bool
+    !!current_user
+  end
+
 end
