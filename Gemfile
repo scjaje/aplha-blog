c306def6ff0239f78379b2c92c09d93e596eaeab
Scott Hardin <shardin2@ncsu.edu>
2023-10-03 12:37:47 -0400

    Tests

--- Gemfile
+++ Gemfile
@@ -38,6 +38,11 @@ gem "bcrypt", "~> 3.1.7"
 
 gem 'will_paginate', '~> 3.1', '>= 3.1.6'
 
+gem 'bootstrap', '~> 5.3.1'
+
+gem 'webpacker'
+
+
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
 
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,11 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import 'bootstrap/dist/js/bootstrap'
+import 'bootstrap/dist/js/bootstrap.bundle'
+import 'jquery'
+import 'bootstrap/dist/js/bootstrap'
+
+document.addEventListener("DOMContentLoaded", () => {
+    // Initialize Bootstrap components here if needed
+})
